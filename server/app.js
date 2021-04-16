74157ef0abef3f341ec92cba91dbd20a5cca015d
noavrd <noavrd@gmail.com>
2021-04-16 18:06:08 +0300

    and post to song

--- server/app.js
+++ server/app.js
@@ -109,4 +109,20 @@ app.get('/album/:id', (req, res) => {
         }
     })
 })
+
+app.post('/song', (req, res) => {
+    let body = req.body;
+    if (body.id && body.songName && body.artistName && body.album && body.length && body.views && body.src && body.lyrics) {
+
+        mysqlCon.query(`INSERT INTO song VALUES (${body.id}, "${body.songName}", "${body.artistName}", "${body.album}", "${body.length}", ${body.views}, "${body.src}", "${body.lyrics}")`, (err, results, fields) => {
+            if (err) {
+                res.send(err.message)
+            } else {
+                res.send("Add successfully")
+            }
+        })
+    } else {
+        res.send("You missed a value")
+    }
+})
 app.listen(3000)
